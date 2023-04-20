9c22ef44ab4a0dced1eb72b080ce733c45c01932
rkuczer <ryan-kooz@hotmail.com>
2023-04-20 12:50:15 -0400

    added validation for record input

--- main.py
+++ main.py
@@ -2,21 +2,34 @@ def calculate_total(state, records):
     # Define tax rates
     tax_rates = {'NJ': 0.066, 'PA': 0.06, 'DE': 0}
 
+    # Check if state is valid
+    if state not in tax_rates:
+        print("Error: Invalid state code. Exiting.")
+        return 0
+
     # Initialize variables
     total = 0.0
     taxable_total = 0.0
     subtotal = 0.0
     tax_rate = tax_rates[state]
 
-    if any(item['price'] < 0 for item in records):
-        print("Error: Negative prices found in record.")
+    #validate item types
+    if any(item['price'] < 0 for item in records) or any(item['price'] == 0 for item in records):
+        print("Error: Invalid price found in item records, exiting.")
         return 0
+
+
 
     # Iterate through the items and calculate total and taxable prices
     for item in records:
+        #validate item type
+        item_type = item.get('type')
+        if item_type not in ['everything else', 'Wic Eligible food', 'Clothing']:
+            print(f"Error: Invalid item type '{item_type}'. Exiting.")
+            return 0
+
         # clothing
         if item['type'] == 'Clothing':
-
             if state == 'PA':
                 # Clothing is tax exempt in PA
                 subtotal += item['price']
@@ -45,64 +58,17 @@ def calculate_total(state, records):
     return max(total, 0)
 
 
-# get the state of the user after their select their records
-def get_state():
-    while True:
-        state = input("Enter your state (DE, PA, NJ): ")
-        if state in ('DE', 'PA', 'NJ'):
-            return state
-        print("Invalid state. Please enter DE, PA, or NJ.")
-
-
-def get_selected_records(records):
-    selected_records = []
-    while True:
-        record_num = input("Enter the line number of the record you want to select, or 'done' if finished: ")
-        if record_num == 'done':
-            break
-        try:
-            record_index = int(record_num) - 1
-            if record_index < 0 or record_index >= len(records):
-                print("Invalid record number. Please try again.")
-            else:
-                selected_records.append(records[record_index])
-                print(f"Selected: {records[record_index]['name']}")
-                print(selected_records)
-        except ValueError:
-            print("Invalid input. Please enter a number or 'done'.")
-    return selected_records
-
-
-# present record options to users
-def print_records(records):
-    for i, item in enumerate(records):
-        print(f"{i + 1}. {item['name']} ({item['type']}, ${item['price']:.2f})")
-
-
 # store inventory for sale
 records = [
-    {'name': 'Sneakers', 'type': 'Clothing', 'price': -50.00},
+    {'name': 'Sneakers', 'type': 'Clothing', 'price': 1.00},
     {'name': 'jeans', 'type': 'Clothing', 'price': 75.00},
     {'name': 'Fur coat', 'type': 'Clothing', 'price': 200.00},
     {'name': 'log', 'type': 'everything else', 'price': 100.00},
     {'name': 'apple', 'type': 'Wic Eligible food', 'price': 100.00}
 ]
 
+#testing code
 state = 'NJ'
 total_charge = calculate_total(state, records)
 print(f'Total charge for state {state}: ${total_charge:.2f}')
 
-# main driver code
-# run = True
-# while run:
-#    print_records(records)
-#    selected_records = get_selected_records(records)
-#    state = get_state()
-#    total_price = calculate_total(state, selected_records)
-#    print(f'Total charge for state {state}: ${total_price:.2f}')
-
-#    user_input = input("Enter 'q' to quit or any other key to continue: ")
-#    if user_input == "q":
-#        run = False
-#        print("Goodbye! Hope you enjoyed my program. ")
-#        exit()
